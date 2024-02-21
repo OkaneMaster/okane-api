e36f3c8a01fc23758582fb9e4bac5fe64d76f430
Yoonsuk Paik <macxpaik@gmail.com>
2024-02-21 22:52:04 +0900

    spring boot

---
+++ src/main/java/com/project/okane/OkaneApplication.java
@@ -0,0 +1,13 @@
+package com.project.okane;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OkaneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OkaneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/okane/OkaneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.okane;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OkaneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
